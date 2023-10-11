704c6e5b009e54bbcf078068e0eb8524b087ac70
thotmx <hermes.o.r@gmail.com>
2023-10-11 13:45:17 -0600

    Indentation stuff

--- lua/config.lua
+++ lua/config.lua
@@ -46,6 +46,9 @@ require 'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+	  enable = true
+  }
 }
 
 -- nvim-tree mapping

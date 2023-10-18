760b2829f0dae06ef886abb4318e3a6677c59a88
thotmx <hermes.o.r@gmail.com>
2023-10-17 22:31:14 -0600

    Add nvim-spectre

--- lua/config.lua
+++ lua/config.lua
@@ -59,9 +59,6 @@ vim.keymap.set('n', '<leader>f', vim.cmd.NvimTreeFindFile, opt)
 -- LazyGit key
 vim.keymap.set('n', '<leader>gg', vim.cmd.LazyGit, opt)
 
-
-
-
 -- Other options
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -78,3 +75,18 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+
+-- Spectre
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
+
